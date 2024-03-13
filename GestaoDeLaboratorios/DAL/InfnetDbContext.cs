dda81313c908fb02b662c84e2a85d85dfbf23ad5
isabella <isabellaoliveira8283@gmail.com>
2024-03-13 13:36:36 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoDeLaboratorios/DAL/InfnetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GestaoDeLaboratorios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoDeLaboratorios.DAL
+{
+    public class InfnetDbContext : DbContext
+    {
+        public InfnetDbContext(DbContextOptions<InfnetDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Computadores> Computadores { get; set; }
+    }
+}
---
+++ GestaoDeLaboratorios/Models/Computadores.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computadores
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Processador { get; set; }
+        [Required]
+        public string PlacaMae { get; set; }
+        [Required]
+        public string Memoria { get; set; }
+        [Required]
+        public string HD { get; set; }
+        [Required]
+        public double NumeroPatrimonio { get; set; }
+        [Required]
+        public string DataCompra { get; set; }
+    }
+}
